84cd77d4666d4065e64e21e626dd49b531c6c22f
UMayhan <topchanyuk29@gmail.com>
2022-03-24 21:41:02 +0200

    GameManager

--- cardGame/Assets/Scripts/WarriorDeck.cs
+++ cardGame/Assets/Scripts/WarriorDeck.cs
@@ -10,15 +10,22 @@ public class WarriorDeck : MonoBehaviour
     //
     // переменные чтобы создать колоду карт для определенного класа с определенного набора карт
     //
+    public int deckSize = 10;
     public List<Card> deck = new List<Card>();
     public int x;
 
     
 
     //private string WarDeck = "Resources/WarDeck";
-    //public int deckSize = 20;
     
-   
+
+    public GameObject cardDeck1;
+    public GameObject cardDeck2;
+    public GameObject cardDeck3;
+    public GameObject cardDeck4;
+
+
+
 
 
     // Start is called before the first frame update
@@ -27,7 +34,7 @@ public class WarriorDeck : MonoBehaviour
         //deck = deckSize;
         x = 0;
         
-        for (int i = 0; i < 20; i++)
+        for (int i = 0; i < 10; i++)
         {
             
             x = Random.Range(1, 4);
@@ -46,6 +53,34 @@ public class WarriorDeck : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (deckSize < 8)
+        {
+            cardDeck1.SetActive(false);
+        }
+        else
+        {
+            cardDeck1.SetActive(true);
+        }
+        if (deckSize < 6)
+        {
+            cardDeck2.SetActive(false);
+        } else
+        {
+            cardDeck2.SetActive(true);
+        }
+        if (deckSize < 3)
+        {
+            cardDeck3.SetActive(false);
+        } else
+        {
+            cardDeck3.SetActive(true);
+        }
+        if (deckSize == 0)
+        {
+            cardDeck4.SetActive(false);
+        } else
+        {
+            cardDeck4.SetActive(true);
+        }
     }
 }
